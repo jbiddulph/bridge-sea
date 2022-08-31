0ad73f4b182cf2bd81fa207c11bb41a6e68205d3
jbiddulph <john.mbiddulph@gmail.com>
2022-08-31 20:03:28 +0100

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="main-content"
+    :style="{
+      'background-size': 'cover',
+      '-webkit-filter': 'blur(8px)',
+      filter: 'blur(8px)',
+      width: '100vw',
+      height: '100vh',
+    }"
+  >
+    <div class="container">
+      <h2>Calculating your total costs</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.container {
+  z-index: 10;
+  filter: blur(0px);
+  -webkit-filter: blur(0px);
+}
+h2 {
+  color: #db2f20;
+}
+.main-content {
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("@/assets/background-office.png");
+}
+</style>
